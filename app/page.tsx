96c80030eb3ae4366929d863cdc772ded1c08266
Sanikk <alexander.petruska@icloud.com>
2024-07-02 23:11:09 +0200

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-import { BrowserRouter as Router } from "react-router-dom";
-import Button from "./components/Button";
-import ButtonGradient from "../public/assets/svg/ButtonGradient";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <Router>
-      <ButtonGradient />
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
-      </div>
-    </Router>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
